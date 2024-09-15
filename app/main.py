0caf297051b671719d5d61b567fca8859d24b764
Federico Vega - 🏠 <federico.vega2222@gmail.com>
2024-09-15 01:18:49 -0300

    feat: requirements //railway port

--- app/main.py
+++ app/main.py
@@ -6,3 +6,8 @@ from pydantic import BaseModel
 app = FastAPI()
 
 app.include_router(score.score_router, prefix='/score', tags=['Score'])
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
